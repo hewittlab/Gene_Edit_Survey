d3de8f8c8263caafcdb1bc62aefe8c4039cb80b4
Paul Sanfilippo <prseye@gmail.com>
2015-06-25 15:21:44 +1000

    merge main and wechat

--- Survey.R
+++ Survey.R
@@ -21,7 +21,10 @@ library(doBy) # Group summary stats
 # CHANGE WORKING DIRECTORY HERE
 
 setwd('~/Dropbox/Research Projects/2015/Gene Edit Survey')
-data <- fromJSON('Answers.json')
+
+# Need 2 files in the working directory: 'Answers.json' and 'wechat.csv'
+wechat <- read.csv(file="wechat.csv", header=T) #wechat data file
+data <- fromJSON('Answers.json') #main data file
 
 # Variable Names
 # 1.createdAt - date/time survey started
@@ -55,8 +58,383 @@ data <- fromJSON('Answers.json')
 # 29.deter_strength - would you use this technology to determine Strength or Sporting ability? Factor variable (levels - N/Y)
 # 30.gen_mod_food - how much do you agree with the use of genetically modified food? Factor variable (levels 1:6)
 
+#WECHAT----------------------------------------------------------------------------------------
 
-#RENAME----------------------------------------------------------------------------------------
+# IMPORT AND PREPARE WECHAT DATA
+
+# RENAME AND REORDER VARIABLES
+
+wechat<-rename(wechat, c("IP地址"="ip", "开始时间"="createdAt", "结束时间"="updatedAt", "Q1_您的性别."="sex", "Q2_您的年龄."="age", "Q3_您所居住的国家."="country", "Q5_有无宗教信仰."="religion", "Q6_您所信仰的宗教是."="religion_type", "Q8_您的受教育水平."="edu_level", "Q9_您之前在保健医疗行业工作过吗."="worked_health", "Q10_您在保健医疗行业所从事的职业是."="worked_health_type", "Q12_您听说过基因工程或基因编辑吗."="heard_about", "Q13_您的经济状况如何."="wealth", "Q14_您家庭里有人有遗传或基因现象吗."="genetic_cond", "Q15_哪些人受到影响."="genetic_cond_affected", "Q16_症状为哪些."="genetic_cond_type", "Q18_..仅治愈儿童或成人自身危及生命的遗传病症.但疾病仍会遗传给下一代."="kids_cure_life", "Q19_.仅治愈儿童或成人使人衰弱的遗传病症.但疾病仍会遗传给下一代."="kids_cure_debil", "Q20_.从胚胎开始根治危及生命的疾病.这就意味着疾病不会被传给下一代."="embr_prev_life", "Q21_......从胚胎开始根治使人衰弱的疾病.这就意味着他们的后代不会患上这种疾病."="embr_prev_debil", "Q22_.利用胚胎改变任何非疾病的特征.例如记忆.瞳色或者身高.这就意味着下一代可能会有同样的基因特征"="edit_for_nondis","Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._外貌.瞳色.发色.肤色."="deter_phys_appear", "Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._智力"="deter_intell", "Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._力量或者体育能力"="deter_strength", "Q25_您是否同意食用转基因食物._您是否同意食用转基因食物."="gen_mod_food"))
+
+# Clean time variables and generate time_to_do variable (minutes)
+time_start <- strptime(wechat$createdAt, format='%Y-%m-%d %H:%M:%S')
+time_end <- strptime(wechat$updatedAt, format='%Y-%m-%d %H:%M:%S')
+wechat$createdAt <- time_start
+wechat$updatedAt <- time_end
+wechat$time_to_do <- round((time_end - time_start)/60,digits=2)
+rm(time_start)
+rm(time_end)
+
+# Generate matching variables to main dataframe
+wechat$language <- as.factor("CHINESE")
+wechat$ethnicity <- as.factor(11)
+wechat$cohort <- as.factor(2)
+wechat$ip <- as.character(wechat$ip)
+
+# Need to remove # from some entried of age variable
+wechat$age <- gsub("#", "", wechat$age)
+wechat$age <- as.numeric(as.character(wechat$age))
+wechat$YOB <- 2015-wechat$age
+
+# Delete unnecessary variables
+wechat$答卷编号 <- wechat$来源 <- wechat$昵称.微信. <- wechat$性别.微信. <- wechat$城市.微信. <- wechat$状态 <- wechat$Q4_请填写您所居住的国家 <- wechat$Q7_请填写您所信仰的宗教名称 <- wechat$Q11_请填写您在保健医疗行业所从事的职业 <- wechat$Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._其他跟健康无关的特征 <- wechat$Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._不做任何改变 <- wechat$Q24_请填写您会改变的其他跟健康无关的特征 <- wechat$Q26_我们对您回答这些的理由很感兴趣.您是否愿意描述一下影响您这些回答的理由或者影响您对于人类基因工程的态度的因素.谢谢 <- wechat$Q27_请填写您的理由 <- NULL
+
+# Reorder
+wechat <- wechat[c("createdAt", "updatedAt", "time_to_do", "cohort", "ip", "language", "country", "sex", "YOB", "age", "ethnicity", "wealth", "edu_level", "worked_health", "worked_health_type", "heard_about", "genetic_cond", "genetic_cond_affected", "genetic_cond_type", "religion", "religion_type", "kids_cure_life", "kids_cure_debil", "embr_prev_life", "embr_prev_debil", "edit_for_nondis", "deter_phys_appear", "deter_intell", "deter_strength", "gen_mod_food")]
+
+
+# RECODE VARIABLES AS NOMINAL/ORDINAL
+
+# COUNTRY
+# Trim Whitespace
+wechat$country <- str_trim(wechat$country, side = "both")
+# Make as Factor
+wechat$country <- as.factor(wechat$country)
+levels(wechat$country)
+# Recode
+wechat$country <- recode(wechat$country,
+'"中国" = "China";
+"澳大利亚" = "Australia";
+"德国" = "Germany";
+"美国" = "United States of America";
+"英国" = "United Kingdom";
+c("其他", "") = "Other"')
+levels(wechat$country)
+
+
+# SEX
+# Trim Whitespace
+wechat$sex <- str_trim(wechat$sex, side = "both")
+# Make as Factor
+wechat$sex <- as.factor(wechat$sex)
+levels(wechat$sex)
+# Recode
+wechat$sex <- recode(wechat$sex,
+'"男" = "M";
+"女" = "F";
+"" = NA')
+levels(wechat$sex)
+
+
+# WEALTH
+# Trim Whitespace
+wechat$wealth <- str_trim(wechat$wealth, side = "both")
+# Make as Factor
+wechat$wealth <- as.factor(wechat$wealth)
+levels(wechat$wealth)
+# Recode
+wechat$wealth <- recode(wechat$wealth,
+'"平均水平之上" = 1;
+"平均水平" = 2;
+"低于平均水平" = 3;
+"" = NA')
+levels(wechat$wealth)
+
+
+# EDU_LEVEL
+# Trim Whitespace
+wechat$edu_level <- str_trim(wechat$edu_level, side = "both")
+# Make as Factor
+wechat$edu_level <- as.factor(wechat$edu_level)
+levels(wechat$edu_level)
+# Recode
+wechat$edu_level <- recode(wechat$edu_level,
+'"小学毕业" = 1;
+"高中毕业" = 2;
+"仅完成大学学业（结业）" = 3;
+"大学毕业" = 4;
+"研究生毕业" = 5;
+"" = NA')
+levels(wechat$edu_level)
+
+
+# WORKED_HEALTH
+# Trim Whitespace
+wechat$worked_health <- str_trim(wechat$worked_health, side = "both")
+# Make as Factor
+wechat$worked_health <- as.factor(wechat$worked_health)
+levels(wechat$worked_health)
+# Recode
+wechat$worked_health <- recode(wechat$worked_health,
+'"无" = "N";
+"有"= "Y";
+"" = NA')
+levels(wechat$worked_health)
+
+
+# WORKED_HEALTH_TYPE
+# Trim Whitespace
+wechat$worked_health_type <- str_trim(wechat$worked_health_type, side = "both")
+# Make as Factor
+wechat$worked_health_type <- as.factor(wechat$worked_health_type)
+levels(wechat$worked_health_type)
+# Recode
+wechat$worked_health_type <- recode(wechat$worked_health_type,
+'"医生" = 1;
+"科研工作者"  = 2;
+"护士" = 3;
+"医学相关专业" = 4;                   
+"医院或其他医疗中心工作者" = 5;
+"其他" = 6;
+"" = NA')
+levels(wechat$worked_health_type)
+
+
+# HEARD_ABOUT
+# Trim Whitespace
+wechat$heard_about <- str_trim(wechat$heard_about, side = "both")
+# Make as Factor
+wechat$heard_about <- as.factor(wechat$heard_about)
+levels(wechat$heard_about)
+# Recode
+wechat$heard_about <- recode(wechat$heard_about,
+'"从未听说过" = 1;
+"听说过一点点" = 2;
+"很了解" =  3') 
+levels(wechat$heard_about)
+
+
+# GENETIC_COND
+# Trim Whitespace
+wechat$genetic_cond <- str_trim(wechat$genetic_cond, side = "both")
+# Make as Factor
+wechat$genetic_cond <- as.factor(wechat$genetic_cond)
+levels(wechat$genetic_cond)
+# Recode
+wechat$genetic_cond <- recode(wechat$genetic_cond,
+'"无" = "N";
+"有"= "Y";
+"" = NA')
+levels(wechat$genetic_cond)
+
+
+# GENETIC_COND_AFFECTED
+# Trim Whitespace
+wechat$genetic_cond_affected <- str_trim(wechat$genetic_cond_affected, side = "both")
+# Make as Factor
+wechat$genetic_cond_affected <- as.factor(wechat$genetic_cond_affected)
+levels(wechat$genetic_cond_affected)
+# Recode
+wechat$genetic_cond_affected <- recode(wechat$genetic_cond_affected,
+'"我" = 1;
+"其他家庭成员"  = 2;
+"我和另外的家庭成员" = 3;
+"" = NA')
+levels(wechat$genetic_cond_affected)
+
+
+# GENETIC_COND_TYPE
+# Trim Whitespace
+wechat$genetic_cond_type <- str_trim(wechat$genetic_cond_type, side = "both")
+# Make as Factor
+wechat$genetic_cond_type <- as.factor(wechat$genetic_cond_type)
+levels(wechat$genetic_cond_type)
+# Recode
+wechat$genetic_cond_type <- recode(wechat$genetic_cond_type,
+'"肌肉萎缩症" = 3;
+"地中海贫血" = 5;
+"唐氏综合症，爱德华综合症，三体综合征" = 9;
+"其他" = 10;
+"" = NA')
+levels(wechat$genetic_cond_type)
+
+
+# RELIGION
+# Trim Whitespace
+wechat$religion <- str_trim(wechat$religion, side = "both")
+# Make as Factor
+wechat$religion <- as.factor(wechat$religion)
+levels(wechat$religion)
+# Recode
+wechat$religion <- recode(wechat$religion,
+'"无" = "N";
+"有"= "Y";
+"" = NA')
+levels(wechat$religion)  
+
+
+# RELIGION_TYPE
+# Trim Whitespace
+wechat$religion_type <- str_trim(wechat$religion_type, side = "both")
+# Make as Factor
+wechat$religion_type <- as.factor(wechat$religion_type)
+levels(wechat$religion_type)
+# Recode
+wechat$religion_type <- recode(wechat$religion_type,
+'"基督教-新教/英国国教" = 1;
+"基督教-天主教" = 2;
+"基督教-其他" = 3;
+"穆斯林" = 4;
+"佛教" = 6;
+"道教" = 9;
+"儒家" = 10;
+"其他" = 14;
+"" = NA')
+levels(wechat$religion_type)
+
+
+# KIDS_CURE_LIFE
+# Trim Whitespace
+wechat$kids_cure_life <- str_trim(wechat$kids_cure_life, side = "both")
+# Make as Factor
+wechat$kids_cure_life <- as.factor(wechat$kids_cure_life)
+levels(wechat$kids_cure_life)
+# Recode
+wechat$kids_cure_life <- recode(wechat$kids_cure_life,
+'"非常同意" = 1;
+"中立" = 3;
+"不同意" = 4;
+"不知道" = 6')
+levels(wechat$kids_cure_life)
+
+
+# KIDS_CURE_DEBIL
+# Trim Whitespace
+wechat$kids_cure_debil <- str_trim(wechat$kids_cure_debil, side = "both")
+# Make as Factor
+wechat$kids_cure_debil <- as.factor(wechat$kids_cure_debil)
+levels(wechat$kids_cure_debil)
+# Recode
+wechat$kids_cure_debil <- recode(wechat$kids_cure_debil,
+'"非常同意" = 1;
+"同意" = 2;
+"中立" = 3;
+"不同意" = 4;
+"坚决反对" = 5;
+"不知道" = 6')
+levels(wechat$kids_cure_debil)
+
+
+# EMBR_PREV_LIFE
+# Trim Whitespace
+wechat$embr_prev_life <- str_trim(wechat$embr_prev_life, side = "both")
+# Make as Factor
+wechat$embr_prev_life <- as.factor(wechat$embr_prev_life)
+levels(wechat$embr_prev_life)
+# Recode
+wechat$embr_prev_life <- recode(wechat$embr_prev_life,
+'"非常同意" = 1;
+"同意" = 2;
+"中立" = 3;
+"不同意" = 4;
+"坚决反对" = 5;
+"不知道" = 6')
+levels(wechat$embr_prev_life)
+
+
+# EMBR_PREV_DEBIL
+# Trim Whitespace
+wechat$embr_prev_debil <- str_trim(wechat$embr_prev_debil, side = "both")
+# Make as Factor
+wechat$embr_prev_debil <- as.factor(wechat$embr_prev_debil)
+levels(wechat$embr_prev_debil)
+# Recode
+wechat$embr_prev_debil <- recode(wechat$embr_prev_debil,
+'"非常同意" = 1;
+"同意" = 2;
+"中立" = 3;
+"不同意" = 4;
+"坚决反对" = 5;
+"不知道" = 6')
+levels(wechat$embr_prev_debil)
+
+
+# EDIT_FOR_NONDIS
+# Trim Whitespace
+wechat$edit_for_nondis <- str_trim(wechat$edit_for_nondis, side = "both")
+# Make as Factor
+wechat$edit_for_nondis <- as.factor(wechat$edit_for_nondis)
+levels(wechat$edit_for_nondis)
+# Recode
+wechat$edit_for_nondis <- recode(wechat$edit_for_nondis,
+'"非常同意" = 1;
+"同意" = 2;
+"中立" = 3;
+"不同意" = 4;
+"坚决反对" = 5;
+"不知道" = 6')
+levels(wechat$edit_for_nondis)
+
+
+# GEN_MOD_FOOD
+# Trim Whitespace
+wechat$gen_mod_food <- str_trim(wechat$gen_mod_food, side = "both")
+# Make as Factor
+wechat$gen_mod_food <- as.factor(wechat$gen_mod_food)
+levels(wechat$gen_mod_food)
+# Recode
+wechat$gen_mod_food <- recode(wechat$gen_mod_food,
+'"非常同意" = 1;
+"同意" = 2;
+"中立" = 3;
+"不同意" = 4;
+"坚决反对" = 5;
+"不知道" = 6')
+levels(wechat$gen_mod_food)
+
+
+# DETER_PYHS_APPEAR
+# Trim Whitespace
+wechat$deter_phys_appear <- str_trim(wechat$deter_phys_appear, side = "both")
+# Make as Factor
+wechat$deter_phys_appear <- as.factor(wechat$deter_phys_appear)
+levels(wechat$deter_phys_appear)
+# Recode
+wechat$deter_phys_appear <- recode(wechat$deter_phys_appear,
+'"" = "N";
+"外貌（瞳色，发色，肤色）" = "Y"')
+levels(wechat$deter_phys_appear)
+
+
+# DETER_INTELL
+# Trim Whitespace
+wechat$deter_intell <- str_trim(wechat$deter_intell, side = "both")
+# Make as Factor
+wechat$deter_intell <- as.factor(wechat$deter_intell)
+levels(wechat$deter_intell)
+# Recode
+wechat$deter_intell <- recode(wechat$deter_intell,
+'"" = "N";
+"智力" = "Y"')
+levels(wechat$deter_intell)
+
+
+# DETER_STRENGTH
+# Trim Whitespace
+wechat$deter_strength <- str_trim(wechat$deter_strength, side = "both")
+# Make as Factor
+wechat$deter_strength <- as.factor(wechat$deter_strength)
+levels(wechat$deter_strength)
+# Recode
+wechat$deter_strength <- recode(wechat$deter_strength,
+'"" = "N";
+"力量或者体育能力" = "Y"')
+levels(wechat$deter_strength)
+
+
+# Print Factors and their levels as check
+PrintLvls <- function(x) 
+{print(data.frame(Lvls=sapply(x[sapply(x, is.factor)], nlevels), 
+  Names=sapply(x[sapply(x, is.factor)], 
+  function(y) paste0(levels(y), collapse=", "))), 
+  right=FALSE) 
+} 
+
+PrintLvls(wechat)
+
+#MAIN----------------------------------------------------------------------------------------
+
+# IMPORT AND PREPARE MAIN DATA
 
 # RENAME AND REORDER VARIABLES
 
@@ -65,7 +443,9 @@ data<-rename(data, c("question_1"="sex", "question_2"="YOB", "question_3"="count
 # Generate age and cohort variables
 data$YOB <- as.numeric(data$YOB)
 data$age <- 2015-data$YOB
-data$cohort <- 1
+data$cohort <- as.factor(1)
+data$country <- as.factor(data$country)
+data$language <- as.factor(data$language)
 
 # Clean time variables and generate time_to_do variable (minutes)
 time_start <- strptime(data$createdAt, format='%Y-%m-%dT%H:%M:%S')
@@ -85,8 +465,6 @@ data$other_traits_alter <- data$question_25 <- data$question_100 <- data$questio
 # Reorder
 data <- data[c("createdAt", "updatedAt", "time_to_do", "cohort", "ip", "language", "country", "sex", "YOB", "age", "ethnicity", "wealth", "edu_level", "worked_health", "worked_health_type", "heard_about", "genetic_cond", "genetic_cond_affected", "genetic_cond_type", "religion", "religion_type", "kids_cure_life", "kids_cure_debil", "embr_prev_life", "embr_prev_debil", "edit_for_nondis", "deter_phys_appear", "deter_intell", "deter_strength", "gen_mod_food")]
 
-
-#RECODE----------------------------------------------------------------------------------------
 
 # RECODE VARIABLES AS NOMINAL/ORDINAL
 
@@ -257,7 +635,7 @@ data$genetic_cond_affected <- recode(data$genetic_cond_affected,
 
 c("Another family member(s)", "فرد)أفراد( آخر فى الأسرة)", "其他家庭成员", "Un autre membre de ma famille", "Ein Familienzugehöriger", "परिवार का अन्य सदस्य", "自分以外の家族メンバー（複数)", "Outro membro (s) da família", "Другой(-ие) член(-ы) семьи", "Otro(s) miembro(s) de mi familia", "Diğer aile üyesi veya üyeleri") = 2;
 
-c("Me and a family member(s)", "أنا وفرد )أفراد( آخر فى الأسرة)", "我和另外的家庭成员 症状为哪些", "Moi et un membre de ma famille", "Ich und meine Familie", "मैं और परिवार का एक सदस्य", "自分と家族（複数）　 病症は何ですか", "Eu e um membro (s) da família ", "Я и член(-ы) семьи", "Yo y un (otros) miembro(s) de mi familia", "Ben ve diğer bir aile üyesi/üyeleri") = 3')
+c("Me and a family member(s)", "أنا وفرد )أفراد( آخر فى الأسرة)", "我和另外的家庭成员 症状为哪些", "Moi et un membre de ma famille", "Ich und meine Familie", "मैं और परिवार का एक सदस्य", "自分と家族（複数）　 病症は何ですか", "自分と家族（複数）　 病", "Eu e um membro (s) da família ", "u e um membro (s) da família", "Я и член(-ы) семьи", "Yo y un (otros) miembro(s) de mi familia", "Ben ve diğer bir aile üyesi/üyeleri") = 3')
 levels(data$genetic_cond_affected)
 
 
@@ -515,15 +893,11 @@ data$deter_strength <- recode(data$deter_strength,
 c("Yes", "نعم", "有", "Oui", "Ja", "हां", "はい", "Sim", "Да", "Sí", "Evet") = "Y"')
 levels(data$deter_strength)
 
-
-# Print Factors and their levels as check
-PrintLvls <- function(x) 
-{print(data.frame(Lvls=sapply(x[sapply(x, is.factor)], nlevels), 
-  Names=sapply(x[sapply(x, is.factor)], 
-  function(y) paste0(levels(y), collapse=", "))), 
-  right=FALSE) 
-  } 
 PrintLvls(data)
+
+#MERGE----------------------------------------------------------------------------------------
+
+all <- rbind(data, wechat)
 
 #GEOFUNC----------------------------------------------------------------------------------------
 
@@ -555,7 +929,7 @@ freegeoip <- function(ip, format = ifelse(length(ip)==1,'list','dataframe'))
 
 # CREATE VECTOR OF IP ADDRESSES, TRIM PRECEDING WHITESPACE AND REMOVE NA'S
 
-ipvec <- data$ip
+ipvec <- all$ip
 ipvec<-str_trim(ipvec, side = "left")
 sum(is.na(ipvec))
 ipvec <- ipvec[!is.na(ipvec)]
@@ -602,9 +976,9 @@ ggsave(file="Bubble_Plot.eps", width=12, height=6)
 # POPULATION PYRAMID
 
 # Set ages above 90 <- NA
-data$age[data$age > 90] <- NA
+all$age[all$age > 90] <- NA
 
-ggplot(data=data,aes(x=age,fill=sex)) + 
+ggplot(data=all,aes(x=age,fill=sex)) + 
   geom_bar(subset=.(sex=="F")) + 
   geom_bar(subset=.(sex=="M"),aes(y=..count..*(-1))) + 
   scale_fill_grey(start = 0.1, end = 0.4, na.value = "grey50") +
