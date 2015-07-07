bb49bcfe1f20cbe5f0583f88f6d8e59714cc2691
Paul Sanfilippo <prseye@gmail.com>
2015-07-08 08:39:49 +1000

    Revert "update script 3: added plot labels"
    This reverts commit 9bfac0ca9d8d2fcdf6fe2956fba1f1c375df5126.

--- 2.IP_address_retrieval.R
+++ 2.IP_address_retrieval.R
@@ -75,8 +75,7 @@ map <- map_data('world')
 str(map)
 ggplot() +
   geom_polygon(data = map, aes(long, lat, group=group), fill="grey50") +
-  geom_point(data = mapdata, aes(x=longitude.m, y=latitude.m, map_id = time_zone, size = latitude.N), col="red") +
-  labs(title="Bubble Plot", x="Longitude", y="Latitude") +
+  geom_point(data = mapdata, aes(x=longitude.m, y=latitude.m, map_id = time_zone, size = latitude.N), col="red")
 ggsave(file="Figures symlink/Bubble_Plot.eps", width=12, height=6)
 
 #----------------------------------------------------------------------------------------
