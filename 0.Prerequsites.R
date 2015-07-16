14be4e62298238b5d1a9c5ea6c85fba4adc5bbf0
George Gooden <gecgooden@gmail.com>
2015-07-16 10:30:04 +0800

    Added file that installs all the required libraries. New libraries should be added to the packages vector

---
+++ 0.Prerequsites.R
@@ -0,0 +1,5 @@
+packages <- c("jsonlite", "stringr", "plyr", "car", "doBy", "maps", "ggplot2", "MASS", "ordinal", "lmtest", "VGAM", "likert")
+print(packages)
+for(i in 1:length(packages)) {
+  install.packages(packages[i])
+}
