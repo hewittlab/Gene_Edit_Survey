dbe82e47f8ab85014dbde9a0119b29b8f46b1030
Paul Sanfilippo <prseye@gmail.com>
2015-07-20 13:41:30 +1000

    update script 4: added likert line plot

--- 4.Plots.R
+++ 4.Plots.R
@@ -2,6 +2,7 @@
 
 library(plyr)
 library(likert)
+library(reshape2)
 library(car) # Recode variables
 library(ggplot2) # Plots
 
@@ -39,6 +40,78 @@ summary(likert_df)
 
 plot(likert_df, ordered=F)
 ggsave(file="Figures symlink/Likert_Bar.eps", width=12, height=10)
+
+
+
+# LIKERT LINE
+
+all_like <- all
+all_like$kids_cure_life <- recode(all_like$kids_cure_life,
+'"1" = "3.Agree";
+"2" = "3.Agree";
+"3" = "2.Neutral";
+"4" = "1.Disagree";
+"5" = "1.Disagree";
+"6" = "2.Neutral"')
+all_like$kids_cure_life <- ordered(all_like$kids_cure_life)
+levels(all_like$kids_cure_life)
+all_like$kids_cure_debil <- recode(all_like$kids_cure_debil,
+'"1" = "3.Agree";
+"2" = "3.Agree";
+"3" = "2.Neutral";
+"4" = "1.Disagree";
+"5" = "1.Disagree";
+"6" = "2.Neutral"')
+all_like$kids_cure_debil <- ordered(all_like$kids_cure_debil)
+levels(all_LR$kids_cure_debil)
+all_like$embr_prev_life <- recode(all_like$embr_prev_life,
+'"1" = "3.Agree";
+"2" = "3.Agree";
+"3" = "2.Neutral";
+"4" = "1.Disagree";
+"5" = "1.Disagree";
+"6" = "2.Neutral"')
+all_like$embr_prev_life <- ordered(all_like$embr_prev_life)
+levels(all_like$embr_prev_life)
+all_like$embr_prev_debil <- recode(all_like$embr_prev_debil,
+'"1" = "3.Agree";
+"2" = "3.Agree";
+"3" = "2.Neutral";
+"4" = "1.Disagree";
+"5" = "1.Disagree";
+"6" = "2.Neutral"')
+all_like$embr_prev_debil <- ordered(all_like$embr_prev_debil)
+levels(all_like$embr_prev_debil)
+all_like$edit_for_nondis <- recode(all_like$edit_for_nondis,
+'"1" = "3.Agree";
+"2" = "3.Agree";
+"3" = "2.Neutral";
+"4" = "1.Disagree";
+"5" = "1.Disagree";
+"6" = "2.Neutral"')
+all_like$edit_for_nondis <- ordered(all_like$edit_for_nondis)
+levels(all_like$edit_for_nondis)
+all_like$gen_mod_food <- recode(all_like$gen_mod_food,
+'"1" = "3.Agree";
+"2" = "3.Agree";
+"3" = "2.Neutral";
+"4" = "1.Disagree";
+"5" = "1.Disagree";
+"6" = "2.Neutral"')
+all_like$gen_mod_food <- ordered(all_like$gen_mod_food)
+levels(all_like$gen_mod_food)
+
+likert_vec <- c("kids_cure_life","kids_cure_debil","embr_prev_life","embr_prev_debil","edit_for_nondis","gen_mod_food")
+likert_df <- all_like[,likert_vec]
+likert_df <- likert_df[complete.cases(all$gen_mod_food),]
+id <- seq(1:length(likert_df$gen_mod_food))
+likert_df <- cbind(likert_df,id)
+likert_df_m <- melt(likert_df, id = "id", value.name="Response", variable.name="Question")
+head(likert_df_m,10)
+
+ggplot(likert_df_m, aes(x = Question, y = Response, group = id)) + 
+  geom_line(position=position_jitter(w=0.15, h=0.15), color=rgb(0,0,0,alpha=0.02)) + geom_point()
+
 
 #POPN----------------------------------------------------------------------------------------
 
