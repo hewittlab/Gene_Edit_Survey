ccb16d52bbc4cc20bc3d96e37637ab13740f2a29
George Gooden <gecgooden@gmail.com>
2015-07-22 15:53:33 +0800

    Removed unnessecary imports and cleaned up executions

--- 1.1.Translate_non_english_free_text.py
+++ 1.1.Translate_non_english_free_text.py
@@ -9,24 +9,22 @@
 # 
 # *Note: This has only been tested on Python 3.4*
 
-# In[25]:
+# In[1]:
 
 import json
-import sys
-import string
 import goslate
 
 
 # ####Used to convert the language encodings from the input file into ones that the Goslate module understands.
 
-# In[26]:
+# In[2]:
 
 langs = {"ENGLISH": "en", "FRENCH":"fr", "CHINESE":"zh-TW", "ARABIC":"ar", "RUSSIAN":"ru", "GERMAN":"de", "HINDI":"hi", "JAPANESE":"ja", "PORTUGUESE":"pt", "SPANISH":"es", "TURKISH":"tr" }
 
 
 # ####Change to your input file
 
-# In[36]:
+# In[3]:
 
 with open("../data/data_with_free_text.json") as f:
     answers = json.load(f)
@@ -35,9 +33,9 @@ with open("../data/data_with_free_text.json") as f:
 # ####Actual Translation
 # Works by calling the Goslate library on the free text boxes that need translating. Filters out the non-english and blank answers to not waste time.
 # 
-# I added the `timeout=100` to the constructor of Goslate as I was getting a timeout error a few minutes in to processing. You may or may not need to tweak this. YMMV.
+# I added the `timeout=100` to the constructor of Goslate as I was getting a timeout error a few minutes in to processing. You may or may not need to tweak this. [YMMV](http://dictionary.cambridge.org/dictionary/british/ymmv).
 
-# In[44]:
+# In[4]:
 
 gs = goslate.Goslate(timeout=100)
 
@@ -52,7 +50,7 @@ for answer in [answer for answer in answers if answer['language'] != "ENGLISH"]:
 
 # ####Write translated data to file
 
-# In[ ]:
+# In[5]:
 
 with open("../data/data_with_free_text_translated.json", "w") as f:
     json.dump(answers, f, indent=4)
