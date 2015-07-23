7da06dcec48a40c31299ccff7f4f63442f8c475d
George Gooden <gecgooden@gmail.com>
2015-07-23 13:58:36 +0800

    Added new prerequsites

--- 0.Prerequsites.R
+++ 0.Prerequsites.R
@@ -1,4 +1,4 @@
-packages <- c("jsonlite", "stringr", "plyr", "car", "doBy", "maps", "ggplot2", "MASS", "ordinal", "lmtest", "VGAM", "likert")
+packages <- c("jsonlite", "stringr", "plyr", "car", "doBy", "maps", "ggplot2", "MASS", "ordinal", "lmtest", "VGAM", "likert", "fifer", "nnet")
 print(packages)
 for(i in 1:length(packages)) {
   install.packages(packages[i])
