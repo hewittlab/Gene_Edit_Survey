8bfc1bf4025e9207d8b340945530c06490e7e5ee
Paul Sanfilippo <prseye@gmail.com>
2015-07-24 10:22:35 +1000

    update script 3: added check for multicollinearity

--- 3.Various_analyses.R
+++ 3.Various_analyses.R
@@ -1856,7 +1856,79 @@ kids_table <- table(all_LR$kids_cure_life)
 
 
 
+# Check Variance Inflation Factor of Predictors
+# https://beckmw.wordpress.com/2013/02/05/collinearity-and-stepwise-vif-selection/
 
+vif_func<-function(in_frame,thresh=10,trace=T,...){
+  require(fmsb)
+  if(class(in_frame) != 'data.frame') in_frame<-data.frame(in_frame)
+  #get initial vif value for all comparisons of variables
+  vif_init<-NULL
+  for(val in names(in_frame)){
+    form_in<-formula(paste(val,' ~ .'))
+    vif_init<-rbind(vif_init,c(val,VIF(lm(form_in,data=in_frame,...))))
+  }
+  vif_max<-max(as.numeric(vif_init[,2]))
+  if(vif_max < thresh){
+    if(trace==T){ #print output of each iteration
+      prmatrix(vif_init,collab=c('var','vif'),rowlab=rep('',nrow(vif_init)),quote=F)
+      cat('\n')
+      cat(paste('All variables have VIF < ', thresh,', max VIF ',round(vif_max,2), sep=''),'\n\n')
+    }
+    return(names(in_frame))
+  }
+  else{
+    in_dat<-in_frame
+    #backwards selection of explanatory variables, stops when all VIF values are below 'thresh'
+    while(vif_max >= thresh){
+      vif_vals<-NULL
+      for(val in names(in_dat)){
+        form_in<-formula(paste(val,' ~ .'))
+        vif_add<-VIF(lm(form_in,data=in_dat,...))
+        vif_vals<-rbind(vif_vals,c(val,vif_add))
+      }
+      max_row<-which(vif_vals[,2] == max(as.numeric(vif_vals[,2])))[1]
+      vif_max<-as.numeric(vif_vals[max_row,2])
+      if(vif_max<thresh) break
+      if(trace==T){ #print output of each iteration
+        prmatrix(vif_vals,collab=c('var','vif'),rowlab=rep('',nrow(vif_vals)),quote=F)
+        cat('\n')
+        cat('removed: ',vif_vals[max_row,1],vif_max,'\n\n')
+        flush.console()
+      }
+      in_dat<-in_dat[,!names(in_dat) %in% vif_vals[max_row,1]]
+    }
+    return(names(in_dat))
+  }
+}
+
+
+vif_test <- all[c(8,10:14,16:17,20:21,31)]
+vif_test <- na.omit(test)
+vif_test$sex <- recode(vif_test$sex,
+'"M" = 1;
+"F" = 2')
+vif_test$worked_health <- recode(vif_test$worked_health,
+'"N" = 1;
+"Y" = 2')
+vif_test$genetic_cond <- recode(vif_test$genetic_cond,
+'"N" = 1;
+"Y" = 2')
+vif_test$religion <- recode(vif_test$religion,
+'"N" = 1;
+"Y" = 2')
+
+vif_test$sex <- as.numeric(vif_test$sex)
+vif_test$ethnicity <- as.numeric(vif_test$ethnicity)
+vif_test$wealth <- as.numeric(vif_test$wealth)
+vif_test$edu_level <- as.numeric(vif_test$edu_level)
+vif_test$worked_health <- as.numeric(vif_test$worked_health)
+vif_test$heard_about <- as.numeric(vif_test$heard_about)
+vif_test$genetic_cond <- as.numeric(vif_test$genetic_cond)
+vif_test$religion <- as.numeric(vif_test$religion)
+vif_test$religion_type <- as.numeric(vif_test$religion_type)
+
+vif_func(in_frame=vif_test,thresh=5,trace=T)
 
 
 
