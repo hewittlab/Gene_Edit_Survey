ac9e5fb7c44e07a97f1b1c341fc853771594fc1c
George Gooden <gecgooden@gmail.com>
2015-08-21 09:48:46 +0800

    Retained the other free text fields

--- 1.0.Data_prep.R
+++ 1.0.Data_prep.R
@@ -61,7 +61,7 @@ data <- fromJSON('Answers.json') #main data file
 
 # RENAME AND REORDER VARIABLES
 
-wechat<-rename(wechat, c("IP地址"="ip", "开始时间"="createdAt", "结束时间"="updatedAt", "Q1_您的性别."="sex", "Q2_您的年龄."="age", "Q3_您所居住的国家."="country", "Q5_有无宗教信仰."="religion", "Q6_您所信仰的宗教是."="religion_type", "Q8_您的受教育水平."="edu_level", "Q9_您之前在保健医疗行业工作过吗."="worked_health", "Q10_您在保健医疗行业所从事的职业是."="worked_health_type", "Q12_您听说过基因工程或基因编辑吗."="heard_about", "Q13_您的经济状况如何."="wealth", "Q14_您家庭里有人有遗传或基因现象吗."="genetic_cond", "Q15_哪些人受到影响."="genetic_cond_affected", "Q16_症状为哪些."="genetic_cond_type", "Q18_..仅治愈儿童或成人自身危及生命的遗传病症.但疾病仍会遗传给下一代."="kids_cure_life", "Q19_.仅治愈儿童或成人使人衰弱的遗传病症.但疾病仍会遗传给下一代."="kids_cure_debil", "Q20_.从胚胎开始根治危及生命的疾病.这就意味着疾病不会被传给下一代."="embr_prev_life", "Q21_......从胚胎开始根治使人衰弱的疾病.这就意味着他们的后代不会患上这种疾病."="embr_prev_debil", "Q22_.利用胚胎改变任何非疾病的特征.例如记忆.瞳色或者身高.这就意味着下一代可能会有同样的基因特征"="edit_for_nondis","Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._外貌.瞳色.发色.肤色."="deter_phys_appear", "Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._智力"="deter_intell", "Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._力量或者体育能力"="deter_strength", "Q24_请填写您会改变的其他跟健康无关的特征"="other_traits_alter", "Q25_您是否同意食用转基因食物._您是否同意食用转基因食物."="gen_mod_food", "Q27_请填写您的理由"="reason"))
+wechat<-rename(wechat, c("IP地址"="ip", "开始时间"="createdAt", "结束时间"="updatedAt", "Q1_您的性别."="sex", "Q2_您的年龄."="age", "Q3_您所居住的国家."="country", "Q5_有无宗教信仰."="religion", "Q6_您所信仰的宗教是."="religion_type", "Q8_您的受教育水平."="edu_level", "Q9_您之前在保健医疗行业工作过吗."="worked_health", "Q10_您在保健医疗行业所从事的职业是."="worked_health_type", "Q12_您听说过基因工程或基因编辑吗."="heard_about", "Q13_您的经济状况如何."="wealth", "Q14_您家庭里有人有遗传或基因现象吗."="genetic_cond", "Q15_哪些人受到影响."="genetic_cond_affected", "Q16_症状为哪些."="genetic_cond_type", "Q18_..仅治愈儿童或成人自身危及生命的遗传病症.但疾病仍会遗传给下一代."="kids_cure_life", "Q19_.仅治愈儿童或成人使人衰弱的遗传病症.但疾病仍会遗传给下一代."="kids_cure_debil", "Q20_.从胚胎开始根治危及生命的疾病.这就意味着疾病不会被传给下一代."="embr_prev_life", "Q21_......从胚胎开始根治使人衰弱的疾病.这就意味着他们的后代不会患上这种疾病."="embr_prev_debil", "Q22_.利用胚胎改变任何非疾病的特征.例如记忆.瞳色或者身高.这就意味着下一代可能会有同样的基因特征"="edit_for_nondis","Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._外貌.瞳色.发色.肤色."="deter_phys_appear", "Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._智力"="deter_intell", "Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._力量或者体育能力"="deter_strength", "Q24_请填写您会改变的其他跟健康无关的特征"="other_traits_alter", "Q25_您是否同意食用转基因食物._您是否同意食用转基因食物."="gen_mod_food", "Q27_请填写您的理由"="reason", "Q11_请填写您在保健医疗行业所从事的职业"="other_health_care", "Q7_请填写您所信仰的宗教名称"="other_religion"))
 
 # Clean time variables and generate time_to_do variable (minutes)
 time_start <- strptime(wechat$createdAt, format='%Y-%m-%d %H:%M:%S')
@@ -84,7 +84,7 @@ wechat$age <- as.numeric(as.character(wechat$age))
 wechat$YOB <- 2015-wechat$age
 
 # Delete unnecessary variables
-wechat$答卷编号 <- wechat$来源 <- wechat$昵称.微信. <- wechat$性别.微信. <- wechat$城市.微信. <- wechat$状态 <- wechat$Q4_请填写您所居住的国家 <- wechat$Q7_请填写您所信仰的宗教名称 <- wechat$Q11_请填写您在保健医疗行业所从事的职业 <- wechat$Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._其他跟健康无关的特征 <- wechat$Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._不做任何改变 <- wechat$Q26_我们对您回答这些的理由很感兴趣.您是否愿意描述一下影响您这些回答的理由或者影响您对于人类基因工程的态度的因素.谢谢 <- NULL #wechat$Q27_请填写您的理由 <- wechat$Q24_请填写您会改变的其他跟健康无关的特征
+wechat$答卷编号 <- wechat$来源 <- wechat$昵称.微信. <- wechat$性别.微信. <- wechat$城市.微信. <- wechat$状态 <- wechat$Q4_请填写您所居住的国家 <- wechat$Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._其他跟健康无关的特征 <- wechat$Q23_如果您可以安全的进行胚胎基因改造.您会使用这项技术改变什么._不做任何改变 <- wechat$Q26_我们对您回答这些的理由很感兴趣.您是否愿意描述一下影响您这些回答的理由或者影响您对于人类基因工程的态度的因素.谢谢 <- NULL #wechat$Q27_请填写您的理由 <- wechat$Q24_请填写您会改变的其他跟健康无关的特征
 
 # Reorder
 wechat <- wechat[c("createdAt", "updatedAt", "time_to_do", "cohort", "ip", "language", "country", "sex", "YOB", "age", "ethnicity", "wealth", "edu_level", "worked_health", "worked_health_type", "heard_about", "genetic_cond", "genetic_cond_affected", "genetic_cond_type", "religion", "religion_type", "kids_cure_life", "kids_cure_debil", "embr_prev_life", "embr_prev_debil", "edit_for_nondis", "deter_phys_appear", "deter_intell", "deter_strength", "gen_mod_food", "other_traits_alter", "reason")]
@@ -446,7 +446,7 @@ levels(wechat$deter_strength)
 
 # RENAME AND REORDER VARIABLES
 
-data<-rename(data, c("question_1"="sex", "question_2"="YOB", "question_3"="country", "question_4"="ethnicity", "question_5"="religion", "question_6"="religion_type", "question_7"="edu_level", "question_8"="worked_health", "question_9"="worked_health_type", "question_10"="heard_about", "question_11"="wealth", "question_12"="genetic_cond", "question_13"="genetic_cond_affected", "question_14"="genetic_cond_type", "question_15"="kids_cure_life", "question_16"="kids_cure_debil", "question_17"="embr_prev_life", "question_18"="embr_prev_debil", "question_19"="edit_for_nondis", "question_20"="deter_phys_appear", "question_21"="deter_intell", "question_22"="deter_strength", "question_23"="other_traits_alter","question_24"="gen_mod_food", "question_25"="reason"))
+data<-rename(data, c("question_1"="sex", "question_2"="YOB", "question_3"="country", "question_4"="ethnicity", "question_5"="religion", "question_6"="religion_type", "question_7"="edu_level", "question_8"="worked_health", "question_9"="worked_health_type", "question_10"="heard_about", "question_11"="wealth", "question_12"="genetic_cond", "question_13"="genetic_cond_affected", "question_14"="genetic_cond_type", "question_15"="kids_cure_life", "question_16"="kids_cure_debil", "question_17"="embr_prev_life", "question_18"="embr_prev_debil", "question_19"="edit_for_nondis", "question_20"="deter_phys_appear", "question_21"="deter_intell", "question_22"="deter_strength", "question_23"="other_traits_alter","question_24"="gen_mod_food", "question_25"="reason", "question_100"="other_religion", "question_101"="other_health_care", "question_102"="other_disease"))
 
 # Fix Arabic and Russian country translations
 Countries_combined <- read.csv("Countries_combined.csv", header=FALSE, stringsAsFactors = FALSE)
@@ -487,10 +487,11 @@ rm(time_end)
 data$client <- data$objectId <- NULL
 
 # Delete free text variables
-data$question_100 <- data$question_101 <- data$question_102 <- NULL
+# other_religion hosp_work_type Other_disease
+# data$question_100 <- data$question_101 <- data$question_102 <- NULL
 
 # Reorder
-data <- data[c("createdAt", "updatedAt", "time_to_do", "cohort", "ip", "language", "country", "sex", "YOB", "age", "ethnicity", "wealth", "edu_level", "worked_health", "worked_health_type", "heard_about", "genetic_cond", "genetic_cond_affected", "genetic_cond_type", "religion", "religion_type", "kids_cure_life", "kids_cure_debil", "embr_prev_life", "embr_prev_debil", "edit_for_nondis", "deter_phys_appear", "deter_intell", "deter_strength", "gen_mod_food", "other_traits_alter", "reason")]
+data <- data[c("createdAt", "updatedAt", "time_to_do", "cohort", "ip", "language", "country", "sex", "YOB", "age", "ethnicity", "wealth", "edu_level", "worked_health", "worked_health_type", "heard_about", "genetic_cond", "genetic_cond_affected", "genetic_cond_type", "religion", "religion_type", "kids_cure_life", "kids_cure_debil", "embr_prev_life", "embr_prev_debil", "edit_for_nondis", "deter_phys_appear", "deter_intell", "deter_strength", "gen_mod_food", "other_traits_alter", "reason", "other_disease", "other_health_care", "other_religion")]
 
 # RECODE VARIABLES AS NOMINAL/ORDINAL
 
